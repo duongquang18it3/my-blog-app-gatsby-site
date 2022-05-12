6739a03f912ab732207d4cb0a32a22cdb78eedf5
Tran Duong Quang <tdquang.18it3@sict.udn.vn>
2022-05-12 17:52:57 +0700

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,16 @@
 module.exports = {
   siteMetadata: {
-    title: "My First Gatsby Site",
+    title: "My First Blog Site by Gatsby",
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      },
+    },
+  ],
 };
